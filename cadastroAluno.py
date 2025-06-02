d95fe9983e1026a8c8b4edaebc53616a2334a3e0
arthur20132009 <arthurfermino008@gmail.com>
2025-06-02 19:58:45 -0300

    cadastroAluno.py

--- cadastroAluno.py
+++ cadastroAluno.py
@@ -6,34 +6,23 @@ import sqlite3
 #  Definicao de Variaveis Globais                    #
 ######################################################
 
-
-### Definicao de funcoes
-
-
-
-
-#######################################################
-#  Definicao de Variaveis Globais                     #
-#######################################################
-def main_menu():
+######################################################
+### Definicao de funcoes                             #
+######################################################
+def main_menu() -> str:
     print("\n Sistema de Cadastro de Alunos")
     print("1. Cadastrar Aluno")
-    print("2, lista de Aluno ")
-    print("3, Atualizar Aluno")
-    print("4, Excluir Aluno")
-    print("5, Sair")
+    print("2. lista de Aluno ")
+    print("3. Atualizar Aluno")
+    print("4. Excluir Aluno")
+    print("5. Sair")
 
-    opcao =("Escolha uma opcao:")
+    opcao: str = input("Escolha uma opção:")
     return opcao
  
-###########################################################
-#  Objetivo: Conectar no Banco de Dados e Criar as Tabelas#
-###########################################################
-
-def CreateTable():
+def Create_table():
     conexao = sqlite3.connect("escola.db")
     cursor = conexao.cursor() 
-
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS aluno( 
@@ -60,14 +49,15 @@ def register(nome,email,idade):
     finally:
         conexao.close()
 
-    conexao.commit()
-    conexao.close()
+
+    
+
 
 def display():
-    conexao = sqlite3.connect("escola.db") 
+    conexao = sqlite3.connect("escola.db") #abrir a conexao com o banco  
     cursor = conexao.cursor() 
 
-    cursor.execute("SELECT * FROM aluno")
+    cursor.execute("SELECT * FROM aluno") 
     aluno = cursor. fetchall()
 
     conexao.close() # fecho a conexao com o banco 
@@ -77,8 +67,20 @@ def display():
     for aluno in aluno:
         print(aluno)
 
+def updade(id,new_nome,new_email,new_idade):
+    
+    conexao = sqlite3.connect("escola.db") #abrir a conexao com o banco  
+    cursor = conexao.cursor() 
+
+    cursor.execute("UPDADE  aluno SET nome = ?,, email = ?, idade = ? WHERE id = ?",
+                   (new_nome,new_email,new_idade,id))
+    
+    conexao.commit() 
+    conexao.close()
+    print("Aluno atualizado com sucesso")
+
 if __name__ == "__main__":
-    CreateTable()
+    Create_table()
         
     while True:
         opcao = main_menu()
@@ -90,16 +92,16 @@ if __name__ == "__main__":
         elif opcao == "2":    
             display()
         elif opcao == "3":
-             id = int(input("Informe o ID do Aluno que vc quer atualizar"))
-            new_nome = input("Novo Nome")
-            new_email = input("Novo Email")
-            new_idade = int(input("Nova Idade"))
-            update(id,new_nome,new_email,new_idade)
+            id = int(input("informe o ID do Aluno que vc quer atualizar"))
+            new_nome  = input ("Novo Nome")
+            new_email = input ("Novo Email")
+            new_idade =  input ("Nova Idade")
         elif opcao == "5":    
             break
         else:
             print("Opcao Invalida") 
 
+print(" Sistema da Cadastro da Aluno")
 
 
 
